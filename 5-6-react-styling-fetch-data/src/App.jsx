1ef1b6185224aefb4f0f1d66dd1ab485f454c544
scrub4 <s202261640@kfupm.edu.sa>
2026-03-04 12:10:15 +0300

    2.1

--- 5-6-react-styling-fetch-data/src/App.jsx
+++ 5-6-react-styling-fetch-data/src/App.jsx
@@ -233,7 +233,7 @@ export default function App() {
      ========================================================= */
   useEffect(() => {
     // TODO 2.2: Implement filtering users here (see lab instructions)
-        if (searchTerm === "") {
+        if (!searchTerm.trim()) {
       setFilteredUsers(users);
     } else {
       const filtered = users.filter((user) =>
