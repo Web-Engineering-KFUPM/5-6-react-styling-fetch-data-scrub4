35cdb36a1857ce7b8ca86e1453a1d0f0be49046b
scrub4 <s202261640@kfupm.edu.sa>
2026-03-04 12:07:28 +0300

    2

--- 5-6-react-styling-fetch-data/src/App.jsx
+++ 5-6-react-styling-fetch-data/src/App.jsx
@@ -204,6 +204,24 @@ export default function App() {
      ========================================================= */
   useEffect(() => {
     // TODO 2.1: Implement fetching users here (see lab instructions)
+    async function fetchUsers() {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
+        const data = await response.json();
+        setUsers(data);
+        setFilteredUsers(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchUsers();
   }, []);
 
   /* =========================================================
@@ -215,6 +233,15 @@ export default function App() {
      ========================================================= */
   useEffect(() => {
     // TODO 2.2: Implement filtering users here (see lab instructions)
+        if (searchTerm === "") {
+      setFilteredUsers(users);
+    } else {
+      const filtered = users.filter((user) =>
+        user.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredUsers(filtered);
+   }
+
   }, [searchTerm, users]);
 
   // Modal handlers (already complete)
